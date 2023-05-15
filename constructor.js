0154239d4e0c9a089366178544f1318fcaf1f525
@MashSHliapa <shliapa.maria@mail.ru>
2023-05-15 14:07:31 +0300

    feat class

--- constructor.js
+++ constructor.js
@@ -1,10 +1,24 @@
 // constructor
-function Todo(title, description, user) {
-  this.id = Date.now(),
-    this.date = new Date().toLocaleDateString(),
-    this.title = title,
-    this.description = description,
-    this.user = user,
-    this.status = 'todo'
+//function Todo(title, description, user) {
+//  this.id = crypto.randomUUID(),
+//    this.date = new Date().toLocaleDateString(),
+//    this.title = title,
+//    this.description = description,
+//    this.user = user,
+//    this.status = 'todo'
+//}
+//export { Todo }
+
+class Todo {
+  id = crypto.randomUUID()
+  date = new Date().toLocaleDateString()
+  status = 'todo'
+
+  constructor (title, description, user) {
+    this.title = title
+    this.description = description
+    this.user = user
+  }
 }
+
 export { Todo }
